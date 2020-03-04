8738ab7e21b0b46c5c76ccebb2eb6fc05584aa7c
Yuu <littleblue19931213@gmali.com>
2020-03-04 01:45:47 +0000

    add styling show templete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was created successfully"
+            flash[:success] = "Article was created successfully"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     def update
         
       if @article.update(article_params)
-          flash[:notice] = "created successfully"
+          flash[:sucess] = "created successfully"
          redirect_to article_path(@article)
       else
           render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = "Deleted!"
+        flash[:danger] = "Deleted!"
         redirect_to articles_path
     end
     
